d85b6555ab55d07e13f1757dc84292785479dd78
isaiful508 <isaiful.islam508@gmail.com>
2024-02-15 18:02:03 +0600

    added play now button working

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,15 @@
+// function play(){
+// //    step 1: hide the home section.to hide screen add the class hidden to the home section
+// const homeSection =document.getElementById('home-screen');
+// homeSection.classList.add('hidden');
+
+// const playGround =document.getElementById('play-ground');
+// playGround.classList.remove('hidden');
+
+// // show the playground
+// }
+
 function play(){
-    console.log('play start now')
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
